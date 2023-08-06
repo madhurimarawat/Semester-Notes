e75dc3de219716ed7afe803ee5ce60ce8a683b80
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2023-08-06 23:58:42 +0530

    Delete 4 SEMESTER/R for Datascience/Lab directory

--- 4 SEMESTER/R for Datascience/Lab/Unit 1/Experiment2.r
+++
@@ -1,7 +0,0 @@
-#Experiment2
-#Create an object X that stores the value then overwrite the object in by itself divided by Y. Print the result to the console.
-x=23
-print(x)
-y=10
-x=x/y
-print(x)
--- 4 SEMESTER/R for Datascience/Lab/Unit 1/Experiment5.r
+++
@@ -1,6 +0,0 @@
-#Experiment5
-#Confirm that the length of the vector created is 20.
-x=10
-y=15.9
-v1=seq(x,y,by=0.3)
-print(length(v1))
